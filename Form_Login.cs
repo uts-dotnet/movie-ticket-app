a3436f6a7f91168b6ded4cc509bd79bb766f7546
Victor Zottmann <zottmann.victor@gmail.com>
2023-10-13 15:37:44 +1100

    update login and sign up on close

--- Form_Login.cs
+++ Form_Login.cs
@@ -12,7 +12,7 @@ namespace MovieTicketApp
             InitializeComponent();
         }
 
-        private void frm_Login_FormClosing(object sender, FormClosingEventArgs e)
+        private void frm_Login_FormClosed(object sender, FormClosedEventArgs e)
         {
             this.Close(); // if the form is not closed it'll cause a conflict between Visual Studio and the executable file
         }
