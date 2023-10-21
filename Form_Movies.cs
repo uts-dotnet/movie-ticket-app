5a9992c85f8a3259dd0d47c0a7637dc9a9ed8eac
Victor Zottmann <zottmann.victor@gmail.com>
2023-10-21 17:23:48 +1100

    add movie session times

--- Form_Movies.cs
+++ Form_Movies.cs
@@ -1,12 +1,14 @@
 ﻿using System;
 using System.Diagnostics;
+using System.Globalization;
 using System.Text.RegularExpressions;
 
 namespace MovieTicketApp
 {
     public partial class frm_Movies : Form
     {
-        private string _moviesFilePath = ".\\movies.txt";
+        private string _moviesFile = ".\\movies.txt";
+        private string _sessionsFile = ".\\sessions.txt";
 
         List<Movie> movies;
 
@@ -14,7 +16,7 @@ namespace MovieTicketApp
         {
             InitializeComponent();
 
-            movies = LoadMovies(_moviesFilePath);
+            movies = LoadMovies(_moviesFile, _sessionsFile);
 
             // this string needs to match the name of a Movie field (i.e. Title, Genre, etc)
             listbox_Movies.DisplayMember = "Title";
@@ -26,15 +28,16 @@ namespace MovieTicketApp
             this.Close();
         }
 
-        public List<Movie> LoadMovies(string filepath)
+        public List<Movie> LoadMovies(string moviesPath, string sessionsPath)
         {
             List<Movie> movies = new List<Movie>();
 
             try
             {
-                string[] lines = File.ReadAllLines(filepath);
+                string[] movieLines = File.ReadAllLines(moviesPath);
+                string[] sessionLines = File.ReadAllLines(sessionsPath);
 
-                for (int i = 1; i < lines.Length; i++)
+                for (int i = 1; i < movieLines.Length; i++)
                 {
                     /*
                      * This regex splits a string by a comma but ignores commas enclosed in " ".
@@ -43,30 +46,47 @@ namespace MovieTicketApp
                      * Reference: Rohit Jain. (2013, September 19). Comment on "Splitting on comma outside quotes." Stack Overflow. https://stackoverflow.com/questions/18893390/splitting-on-comma-outside-quotes
                      */
                     string regex = ",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)";
-                    string[] data = Regex.Split(lines[i], regex);
+                    string[] movieData = Regex.Split(movieLines[i], regex);
+                    int movieId = Convert.ToInt32(movieData[0]);
 
+                    string sessions = sessionLines.FirstOrDefault(line => line.StartsWith(movieId.ToString()));
 
-                    int id = Convert.ToInt32(data[0]);
-                    string title = data[1];
-                    string genre = data[2];
-                    int hours = Convert.ToInt32(data[3]);
-                    int minutes = Convert.ToInt32(data[4]);
-                    int year = Convert.ToInt32(data[5]);
-                    int month = Convert.ToInt32(data[6]);
-                    int day = Convert.ToInt32(data[7]);
-                    string description = data[8].Trim('"'); // remove the double quotes from the description
-                    string poster = data[9];
+                    try
+                    {
+                        string[] sessionData = sessions.Split(',');
+                        int availableSeats = Convert.ToInt32(sessionData[sessionData.Length - 1]); // get the available seats (last item)
 
-                    Duration duration = new Duration(hours, minutes);
-                    DateTime releasedate = new DateTime(year, month, day);
-                    Movie movie = new Movie(id, title, genre, duration, releasedate, description, poster);
+                        string title = movieData[1];
+                        string genre = movieData[2];
+                        int hours = Convert.ToInt32(movieData[3]);
+                        int minutes = Convert.ToInt32(movieData[4]);
+                        int year = Convert.ToInt32(movieData[5]);
+                        int month = Convert.ToInt32(movieData[6]);
+                        int day = Convert.ToInt32(movieData[7]);
+                        string description = movieData[8].Trim('"'); // remove the double quotes from the description
+                        string poster = movieData[9];
 
-                    movies.Add(movie);
+                        Duration duration = new Duration(hours, minutes);
+                        DateTime releasedate = new DateTime(year, month, day);
+                        Movie movie = new Movie(movieId, title, genre, duration, releasedate, description, poster);
+
+                        for (int j = 1; j < sessionData.Length - 1; j++) // Start at index 1 to skip the movie ID and last time (available seats)
+                        {
+                            DateTime sessionTime = DateTime.ParseExact(sessionData[j], "H:mm", CultureInfo.InvariantCulture);
+                            movie.AddSession(sessionTime, availableSeats);
+                        }
+
+                        movies.Add(movie);
+                    }
+                    catch (Exception e)
+                    {
+                        MessageBox.Show(e.Message);
+                    }
                 }
             }
             catch (FileNotFoundException e)
             {
-                Debug.WriteLine(e.Message);
+                MessageBox.Show(e.Message);
             }
 
             return movies;
@@ -93,6 +113,27 @@ namespace MovieTicketApp
                 catch (Exception ex)
                 {
                     Debug.WriteLine($"An error occurred: {ex.Message}");
+                }
+
+                Button[] sessionBtns = new Button[]
+                {
+                    btn_Session_Time_1,
+                    btn_Session_Time_2,
+                    btn_Session_Time_3,
+                    btn_Session_Time_4,
+                    btn_Session_Time_5,
+                };
+
+                int x = 266; // x position of the first button (it increments in the for loop)
+                int y = 381; // y position of all the buttons (it doesn't increment in the for loop)
+
+                for (int i = 0; i < selectedMovie.Sessions.Count && i < sessionBtns.Length; i++)
+                {
+                    sessionBtns[i].Text = selectedMovie.Sessions[i].Date.ToString("HH:mm");
+                    sessionBtns[i].Tag = selectedMovie.Sessions[i];
+
+                    sessionBtns[i].Location = new Point(x, y);
+                    x += 106; // increase the x position of each subsequent button by 106px
                 }
             }
         }
---
+++ src/MovieSession/MovieSession.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace MovieTicketApp
+{
+    public class MovieSession
+    {
+        public DateTime Date { get; private set; }
+        public int AvailableSeats { get; private set; }
+
+        public MovieSession(DateTime date, int availableSeats)
+        {
+            this.Date = date;
+            this.AvailableSeats = availableSeats;
+        }
+    }
+}
