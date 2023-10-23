7e08e6ec9366f692c298ee99bec5baf8314c4f9a
Emily Deng <exd154@case.edu>
2023-10-23 15:11:44 +1100

    Add user profile and files

---
+++ Form_ChangePassword.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace MovieTicketApp
+{
+    partial class frm_ChangePassword
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            lbl_Title_SignUp = new Label();
+            lbl_FirstName = new Label();
+            label2 = new Label();
+            label1 = new Label();
+            label3 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            label4 = new Label();
+            label5 = new Label();
+            SuspendLayout();
+            // 
+            // lbl_Title_SignUp
+            // 
+            lbl_Title_SignUp.AutoSize = true;
+            lbl_Title_SignUp.Font = new Font("Segoe UI", 21.75F, FontStyle.Bold, GraphicsUnit.Point);
+            lbl_Title_SignUp.ForeColor = Color.Firebrick;
+            lbl_Title_SignUp.Location = new Point(263, 22);
+            lbl_Title_SignUp.Name = "lbl_Title_SignUp";
+            lbl_Title_SignUp.Size = new Size(258, 40);
+            lbl_Title_SignUp.TabIndex = 2;
+            lbl_Title_SignUp.Text = "Change Password";
+            // 
+            // lbl_FirstName
+            // 
+            lbl_FirstName.AutoSize = true;
+            lbl_FirstName.BackColor = Color.Firebrick;
+            lbl_FirstName.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            lbl_FirstName.ForeColor = SystemColors.ButtonFace;
+            lbl_FirstName.Location = new Point(34, 37);
+            lbl_FirstName.Name = "lbl_FirstName";
+            lbl_FirstName.Size = new Size(69, 21);
+            lbl_FirstName.TabIndex = 14;
+            lbl_FirstName.Text = "Go Back";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            label2.Location = new Point(244, 120);
+            label2.Name = "label2";
+            label2.Size = new Size(138, 21);
+            label2.TabIndex = 18;
+            label2.Text = "Current Password";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.Location = new Point(244, 205);
+            label1.Name = "label1";
+            label1.Size = new Size(116, 21);
+            label1.TabIndex = 19;
+            label1.Text = "New Password";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            label3.Location = new Point(244, 288);
+            label3.Name = "label3";
+            label3.Size = new Size(142, 21);
+            label3.TabIndex = 20;
+            label3.Text = "Confirm Password";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(244, 144);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(298, 23);
+            textBox1.TabIndex = 21;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(244, 229);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(298, 23);
+            textBox2.TabIndex = 22;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(244, 312);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(298, 23);
+            textBox3.TabIndex = 23;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.BackColor = Color.Firebrick;
+            label4.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            label4.ForeColor = SystemColors.ButtonFace;
+            label4.Location = new Point(347, 373);
+            label4.Name = "label4";
+            label4.Size = new Size(62, 21);
+            label4.TabIndex = 24;
+            label4.Text = "Submit";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            label5.Location = new Point(312, 62);
+            label5.Name = "label5";
+            label5.Size = new Size(157, 21);
+            label5.TabIndex = 25;
+            label5.Text = "At least 4 characters";
+            // 
+            // frm_ChangePassword
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label3);
+            Controls.Add(label1);
+            Controls.Add(label2);
+            Controls.Add(lbl_FirstName);
+            Controls.Add(lbl_Title_SignUp);
+            Name = "frm_ChangePassword";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label lbl_Title_SignUp;
+        private Label lbl_FirstName;
+        private Label label2;
+        private Label label1;
+        private Label label3;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private Label label4;
+        private Label label5;
+    }
+}
---
+++ Form_UserProfile.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MovieTicketApp
+{
+    public partial class Form_UserProfile : Form
+    {
+
+        string[] dataArray = { "Button 1", "Button 2", "Button 3", "Button 4", "Button 5" };
+
+
+        public Form_UserProfile()
+        {
+
+            foreach (string item in dataArray)
+            {
+                Button button = new Button
+                {
+                    Text = item,
+                    Width = 100, // Set the width as per your preference
+                    Height = 30   // Set the height as per your preference
+                };
+
+                button.Click += Button_Click; // Add a click event handler for each butt
+                //panel_MovieButtons.Controls.Add(button); // Add the button to the FlowLayoutPanel
+            }
+
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, EventArgs e)
+        {
+            //Add code to open the movie ticket view here!
+
+            // Handle the button click event here
+            // You can access the button's Text property to identify which button was clicked
+            Button clickedButton = (Button)sender;
+            MessageBox.Show("You clicked: " + clickedButton.Text);
+        }
+
+
+        private void btn_ChangePassword_Click(object sender, EventArgs e)
+        {
+            frm_ChangePassword changePasswordForm = new frm_ChangePassword();
+            changePasswordForm.Show();
+            this.Hide();
+        }
+
+        private void Form_UserProfile_Load(object sender, EventArgs e)
+        {
+            // Populate listBox1 when the form loads
+            PopulateListBox();
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // Handle the event when an item in listBox1 is selected
+            string selectedBooking = listBox1.SelectedItem as string;
+
+            if (selectedBooking != null)
+            {
+                // You can perform some action based on the selected booking here
+                MessageBox.Show("You selected: " + selectedBooking);
+            }
+        }
+
+        private void PopulateListBox()
+        {
+            string filePath = "bookings.txt"; // Replace with the actual file path
+
+            if (File.Exists(filePath))
+            {
+                // Read the file's content line by line and add it to listBox1
+                string[] lines = File.ReadAllLines(filePath);
+
+                foreach (string line in lines)
+                {
+                    listBox1.Items.Add(line);
+                }
+            }
+            else
+            {
+                MessageBox.Show("The file does not exist.");
+            }
+        }
+    }
+}
